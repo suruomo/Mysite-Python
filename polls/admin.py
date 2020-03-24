94967fbdeecfc7879256880f13ba6829f795fca4
suruomo <www.suruomo@hotmail.com>
2020-03-24 15:56:30 +0800

    自定义后台管理页面

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-
 from .models import Question
 from .models import Choice
-# Question对象具有管理界面
-admin.site.register(Question)
+
+# 自定义后台管理样式
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # 页面显示
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # 字段分集管理
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+# Question,Choice对象具有管理界面
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
